8a0cf7663b095ecfc582b228d0d7ff794d48295b
phantom05 <monster2jy@gmail.com>
2020-01-21 00:55:38 +0900

    업데이트 : 선택정렬

---
+++ 선택정렬/index.js
@@ -0,0 +1,18 @@
+// 선택정렬
+let temp,index,i,j,min;
+const array = [10,9,7,2,1,3,6,5,4];
+
+for(i = 0; i < array.length; i ++){
+  min=9999;
+  for(j = i; j < array.length; j++){
+    if(min > array[j]){
+      min = array[j];
+      index = j;
+    }
+  }
+  temp = array[i];
+  array[i] = array[index];
+  array[index] = temp
+}
+
+console.log(array);
